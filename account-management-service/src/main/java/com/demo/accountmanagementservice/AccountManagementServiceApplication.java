dc4b991e0b1c0dee10de9e8fe7f5fd98f9ed3f72
m.baset@zoho.com <m.baset@zoho.com>
2023-05-23 17:30:36 +0300

    project setup: creating account-management, inventory, order, and notification services templates

---
+++ account-management-service/src/main/java/com/demo/accountmanagementservice/AccountManagementServiceApplication.java
@@ -0,0 +1,12 @@
+package com.demo.accountmanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountManagementServiceApplication.class, args);
+	}
+}
